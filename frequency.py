b136dfd42bedda025ab8cb5d0f581428955a1422
Christopher Brooks <christopherbrooks@Ada.local>
2018-02-15 14:07:54 -0500

    going to change list to number

---
+++ frequency.py
@@ -0,0 +1,26 @@
+scale = ['bes ', 'd ', 'e ', 'f ', 'g ', 'aes ', 'b ', 'c ']
+print scale[0]
+
+frequencies = {
+   'c ': 130.81, 
+   'cis ': 138.59,
+   'des ': 138.59,
+   'd ': 146.83,
+   'dis ': 155.56,
+   'ees ': 155.56,
+   'e ': 164.81,
+   'f ': 174.61, 
+   'fis ': 185.00,
+   'ges ': 185.00,
+   'g ': 196.00,
+   'gis ': 207.65,
+   'aes ': 207.65,
+   'a ': 220.00,
+   'ais ': 233.08,
+   'bes ': 233.08,
+   'b ': 246.94,
+   'c ': 261.63,
+ }
+
+tonic_frequency = frequencies[scale[0]]
+print tonic_frequency
--- scale.py
+++ scale.py
@@ -1,4 +1,6 @@
-# defines the notes of a scale
+from __future__ import division
+import math
+from pyaudio import PyAudio  # sudo apt-get install python{,3}-pyaudio
 import random
 
 def scale(number_of_octaves):
@@ -16,29 +18,93 @@ def scale(number_of_octaves):
 	type_of_scale = random.sample(types_of_scales, 1)
 	print type_of_scale[0]
 	full_scale_list = scale.get(type_of_scale[0])
-	full_scale = ''.join(full_scale_list)
-
+	
 # scale can start on any note
-	n = random.randint(0, len(full_scale) - 2)
-	print n
-	if n % 2 == 0: # n is even
-		full_scale = full_scale[n:] + full_scale[2: n + 1]
-	else: #n is odd
-		full_scale = full_scale[n + 1:] + full_scale[2: n + 2]
+	n = random.randint(0, len(full_scale_list) - 1)
+	full_scale_list = full_scale_list[n:] + full_scale_list[1 : n]
 
 # scale can have 1 through 4 octaves
-	if number_of_octaves == 0:
-		return full_scale
-	else:
-		for i in range(1, number_of_octaves):
-			full_scale = full_scale + full_scale [1:]
+	for i in range(1, number_of_octaves):
+		full_scale_list = full_scale_list + full_scale_list
+	full_scale_list = full_scale_list + full_scale_list[0 : 1]
+# return frequency of tonic
+	
+	frequencies = {
+   'c ': 130.81, 
+   'cis ': 138.59,
+   'des ': 138.59,
+   'd ': 146.83,
+   'dis ': 155.56,
+   'ees ': 155.56,
+   'e ': 164.81,
+   'f ': 174.61, 
+   'fis ': 185.00,
+   'ges ': 185.00,
+   'g ': 196.00,
+   'gis ': 207.65,
+   'aes ': 207.65,
+   'a ': 220.00,
+   'ais ': 233.08,
+   'bes ': 233.08,
+   'b ': 246.94,
+   'c ': 261.63,
+ }
 
-	return full_scale
+	tonic_frequency = frequencies[full_scale_list[0]]
+	#print full_scale_list[0]
+	
+	
+	# print "tonic: ", scale.keys()
 
-# input number of octave and call scale function
+	#print full_scale_list
+# returns scale as string
+	full_scale = ''.join(full_scale_list)
+
+	return full_scale, tonic_frequency
+
+
+# play a fixed frequency sound
+def sine_tone(frequency, duration, volume=1, sample_rate=22050):
+    assert isinstance(frequency, float)
+    assert isinstance(duration, int)
+
+    n_samples = sample_rate * duration
+    restframes = n_samples % sample_rate
+
+    p = PyAudio()
+    try:
+        stream = p.open(format=p.get_format_from_width(1),  # 8bit
+                        channels=1,  # mono
+                        rate=sample_rate,
+                        output=True)
+
+        def s(t):
+            wave = volume * math.sin(2 * math.pi * frequency * t / sample_rate)
+            return int(wave * 0x7f + 0x80)
+
+        full_range = range(0, n_samples, sample_rate)  # 0, n_samples, 2*n_samples, ... , sample_rate-1
+        for i in full_range:
+            partial_range = range(i, i + sample_rate)  # i, i+1, ..., i+sample_rate-1
+            buf = map(s, partial_range)  # apply s to every int in partial range
+            stream.write(bytes(bytearray(buf)))
+
+        # fill remainder of frameset with silence
+        stream.write(b'\x80' * restframes)
+        
+
+    finally:
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+
+# Main Body of Program input number of octave and call scale function
 while True:
 	number_of_octaves = input("How many octaves?")
 	if 0 < number_of_octaves <= 4:
 		break
-print number_of_octaves
-print scale(number_of_octaves)
+full_scale = scale(number_of_octaves)
+# scale notes
+print full_scale[0]
+# pitch of tonic
+sine_tone(full_scale[1], 3)
