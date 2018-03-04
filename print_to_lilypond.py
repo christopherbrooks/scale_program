0d794e4ebca889ef2d5469c405b549a6603d7f0c
Christopher Brooks <christopherbrooks@Ada.local>
2018-03-03 19:49:19 -0500

    how to pop open the graphic?

--- print_to_lilypond.py
+++ print_to_lilypond.py
@@ -1,16 +1,18 @@
 import os
 
+os.remove("lily_scale.ly")
+
 rhythmic_pattern = ('a', 3, 4)
-scale = 'a b c d e f g'
+scale = 'a b c des e fis g'
 full_scale = ('acoustic', '1', 'c')
 
 lilyfile = open('lily_scale.ly', 'a') 
 lilyfile.write('\\version \"2.18.2\" \n')
-lilyfile.write('\\relative c'' \n')
+lilyfile.write("\\relative c' \n")
 lilyfile.write('{ \n')
 lilyfile.write('\\time \n')
 lilyfile.write('{}/{} \n'.format(int(rhythmic_pattern[1]), rhythmic_pattern[2]))
-lilyfile.write('{} \n'.format(scale)
+lilyfile.write('{}\n'.format(scale))
 lilyfile.write('} \n')
 lilyfile.write('\\header { \n')
 lilyfile.write('subtitle = \"{}, tonic: {}\"\n'.format(full_scale[2], full_scale[0][0]))
@@ -21,4 +23,10 @@ lilyfile = open('lily_scale.ly', 'r')
 print lilyfile.read()
 lilyfile.close()
 
+os.system("lilypond lily_scale.ly")
+
+import webbrowser
+
+#looking for a way to pop open a pdf reader
+webbrowser.open_new(r'lily_scale.pdf')
 
