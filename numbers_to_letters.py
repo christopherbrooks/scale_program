c72f03a97a707ab6288b534ad8aacc4b19ba9e15
Ethan Brooks <ethanabrooks@gmail.com>
2018-03-18 20:19:19 -0400

    made changes

--- numbers_to_letters.py
+++ numbers_to_letters.py
@@ -2,37 +2,34 @@ scale = [8, 10, 11, 2, 3, 5, 7]
 print scale
 
 # note names with more common accidentals
-names = {
-   0 : 'c ', # c
-   1 : 'cis ',  # c#, db
-   2 : 'd ', # d
-   3 : 'ees ', # d# eb
-   4 : 'e ', # e
-   5 : 'f ', # f
-   6 : 'fis ', # f# gb
-   7 : 'g ', # g
-   8 : 'aes ', # g# ab
-   9 : 'a ', # a
-   10 : 'bes ', # a# bb
-   11 : 'b ', # b
-   12 : 'c ', # c
- }
+names = [
+    'c ',  # c
+    'cis ',  # c#, db
+    'd ',  # d
+    'ees ',  # d# eb
+    'e ',  # e
+    'f ',  # f
+    'fis ',  # f# gb
+    'g ',  # g
+    'aes ',  # g# ab
+    'a ',  # a
+    'bes ',  # a# bb
+    'b ',  # b
+    'c ',  # c
+]
 
 alternate_accidentals = {
-   1 : 'des ',  # c#, db
-   3 : 'dis ', # d# eb
-   6 : 'get ', # f# gb
-   8 : 'gis ', # g# ab
-   10 : 'ais ', # a# bb
- }
+    1: 'des ',  # c#, db
+    3: 'dis ',  # d# eb
+    6: 'get ',  # f# gb
+    8: 'gis ',  # g# ab
+    10: 'ais ',  # a# bb
+}
 
 n = 0
-scale_spelled = []
-while n < len(scale):
-	scale_spelled.append(names[scale[n]])
-	if scale_spelled[n][0] == scale_spelled[n - 1][0]:
-		print scale_spelled[n]
-		
-	n = n + 1
+scale_spelled = [names[s] for s in scale]
+for n in range(len(scale)):
+    if scale_spelled[n][0] == scale_spelled[n - 1][0]:
+        print scale_spelled[n]
 
 print scale_spelled
--- rhythm.py
+++ rhythm.py
@@ -1,52 +1,37 @@
-
 import random
 
 
 def rhythm(scale):
-	rhythmic_subdivisions = {2: "2 ", 1.5: "4. ", 1.0: "4 ", 0.75: "8. ", 0.5: "8 ", 0.25: "16 "}
+    rhythmic_subdivisions = {2: 2, 1.5: 4., 1.0: 4, 0.75: 8., 0.5: 8, 0.25: 16}
 
-# number of notes in rhythmic subdivision has to be even divisor of number of notes in scale
-	number_of_notes = random.randint(1,8)
+    # number of notes in rhythmic subdivision has to be even divisor of number of notes in scale
+    divisors = [i for i in range(1, 8) if len(scale) % i == 0]
+    number_of_notes = random.choice(divisors)
 
-	while len(scale) % number_of_notes != 0:
-		number_of_notes = random.randint(1,8)
-	
-	rhythmic_pattern = []
-	pattern_duration = 0
+    random_choices = random.sample(rhythmic_subdivisions.items(),
+                                   number_of_notes)
+    """
+    zip converts `[(a, 1), (b, 2), ...]` to `([a, b, ...], [1, 2, ...])`
+    `f(*[a, b, c])` means `f(a, b, c)`. In other words, `*` tells a function
+    to treat every element of the input as a separate argument
+    """
+    rhythmic_pattern, durations = zip(*random_choices)
+    rhythmic_pattern = [random.choice(rhythmic_subdivisions)
+                        for _ in range(number_of_notes)]
+    pattern_duration = sum(durations)
 
-	for l in range(number_of_notes):
-		k = random.randint(0, len(rhythmic_subdivisions)-1)
-		note_value = rhythmic_subdivisions.values()[k]
-		rhythmic_pattern.append(note_value)
-		note_duration = rhythmic_subdivisions.keys()[k]
-		pattern_duration = pattern_duration + note_duration
+    # figure time signature
 
-# figure time signature
+    while pattern_duration % .5 != 0:
+        pattern_duration += pattern_duration
 
-	while pattern_duration % .5 != 0:
-		pattern_duration += pattern_duration
-
-	if pattern_duration % 1 == 0:
-		beats_per_bar = pattern_duration
-		beat = 4
-	elif pattern_duration % .5 == 0:
-		beats_per_bar = pattern_duration * 2
-		beat = 8
-	else:
-		beats_per_bar = pattern_duration * 4
-		beat = 16
-
-	return rhythmic_pattern, beats_per_bar, beat
-
+    multiple = 1 if pattern_duration % 1 == 0 else 2
+    beats_per_bar = rhythmic_pattern * multiple
+    beat = 4 * multiple
+    return rhythmic_pattern, beats_per_bar, beat
 
 
 scale = ['a ', 'b ', 'c ', 'd ', 'ees ', 'f ', 'fis ', 'aes ', 'a ']
-#scale = scale + scale[::-1]
+# scale = scale + scale[::-1]
 
 print rhythm(scale)
-
-
-
-
-
-
